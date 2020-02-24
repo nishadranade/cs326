9fef8b11fea1e6856efe2785d772b1c2a20e4720
nishadranade <nranade@umass.edu>
2020-02-24 16:53:41 -0500

    typescript intro 5 minutes

--- greeter.ts
+++ greeter.ts
@@ -1,1 +1,19 @@
-// nishad ranade - 2020
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
